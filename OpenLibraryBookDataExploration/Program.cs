61e34f91e0cf4bbadc9cb387f991e5a5587f8fe3
Joshua Skipper <yasoiskipper1@outlook.com>
2025-08-30 20:17:17 -0400

    Add project files.

---
+++ OpenLibraryBookDataExploration/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.Text.Json;
+
+namespace OpenLibraryBookDataExploration
+{
+    class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Today.ToString("D"));
+
+
+            string url = "https://pokeapi.co/api/v2/pokemon?limit=200&utm_source=chatgpt.com";
+
+            HttpClient client = new HttpClient();
+
+            try
+            {
+                var response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error:{ex.Message}");
+            }
+        }
+    }
+}
